d192196ec938f51c4cf11ca5fbb641a8ab147270
Özgür KILIÇ <ozgurkilic147@gmail.com>
2025-02-05 00:47:40 +0100

    Tutorial Finished

--- Program.cs
+++ Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            var key = Console.ReadKey();
+            if (key.Key == ConsoleKey.R) 
+            {
+                throw new CustomException("Bu cındır R tuşuna bastı!");
+            }
+        }
+        catch (CustomException Exp) 
+        {
+            Console.WriteLine(Exp);
+            Console.WriteLine(Exp.Message);
+        }
+    }
+}
+
+class CustomException : Exception 
+{
+    public CustomException(string msg) : base(msg) 
+    {
+
     }
 }
